20f9450f695b33fc48ff0a8fba01da237efb95fe
aaronbiscotti <thesoupmanguy@gmail.com>
2023-01-10 14:00:29 -0500

    mobile

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="fixed py-10 bottom-0 flex flex-col justify-center items-center">
+            <h1>Copyright © 2023, Aaron & Co.</h1>
+            <p className="text-[8px] max-w-[300px]">Software management & services</p>
+        </div>
+    )
+}
+
+export default Footer
